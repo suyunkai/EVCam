1a448d72ccc5b54f5495c75929a78a58df95d985
suyunkai <cn.syk@foxmail.com>
2026-01-30 18:18:47 +0800

    Release v0.9.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.kooo.evcam"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "0.9.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
