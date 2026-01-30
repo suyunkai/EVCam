868276789df6a17a522ff279328ccf0a141c64a1
ElliotWu <112760141+Elliotwu-7@users.noreply.github.com>
2026-01-30 23:28:32 +0800

    For LK07

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 12
-        versionName = "0.9.9-test-01302215"
+        versionName = "0.9.9-test-01302328"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
