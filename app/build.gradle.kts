b5c5ce9f61dd553450876cda11845357db4736db
suyunkai <cn.syk@foxmail.com>
2026-02-08 16:14:59 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.kooo.evcam"
         minSdk = 28
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.0.7-test-02081226"
+        versionCode = 21
+        versionName = "1.0.8"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
